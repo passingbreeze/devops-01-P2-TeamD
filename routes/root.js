3ed5cb3d1ac4148a9423a458c6f2e3efe5a4e52a
crowss-nest <wise.arcn@gmail.com>
2022-03-31 15:33:37 +0900

    fix conflic

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,10 @@
 'use strict'
 
+const { requireServerPluginFromPath } = require("fastify-cli/util")
+
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+   reply.code(200)
+   .send ('hello world!')
   })
 }
