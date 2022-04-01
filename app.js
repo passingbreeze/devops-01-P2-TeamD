48260c5efc1bcca1b6ce9c332ea302222804cc4d
ByeongJunis <guqudjun11@naver.com>
2022-04-02 00:27:52 +0900

    commit

--- app.js
+++ app.js
@@ -23,12 +23,5 @@ module.exports = async function (fastify, opts) {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
   })
-
-  fastify.register(require('fastify-mongodb'), {
-    // force to close the mongodb connection when app stopped
-    // the default value is false
-    forceClose: true,
   
-    url: process.env.MONGODB_ENDPOINT
-  })
 }
