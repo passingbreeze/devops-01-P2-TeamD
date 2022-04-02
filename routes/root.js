4fe30a93ad4c94de1f62afe7d6e7af9f1c604aea
passingbreeze <jungmin1237@pusan.ac.kr>
2022-04-02 23:34:45 +0900

    feat: add server status message

--- routes/root.js
+++ routes/root.js
@@ -4,6 +4,9 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return reply
       .code(200)
-      .send(`Hello, World!`)
+      .send(`<div class="mt-4 p-5 bg-primary text-white rounded">
+      <h1>현재 서버가 정상 작동중입니다.</h1>
+      <p>데이터베이스도 연결되었습니다...</p>
+    </div>`)
   })
 }
