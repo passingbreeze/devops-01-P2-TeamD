3adcbff53696d135d4e1e598cc61881ffb06f0b5
passingbreeze <jungmin1237@pusan.ac.kr>
2022-04-03 00:13:01 +0900

    feat: add server status message

--- routes/root.js
+++ routes/root.js
@@ -4,9 +4,25 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return reply
       .code(200)
-      .send(`<div class="mt-4 p-5 bg-primary text-white rounded">
-      <h1>현재 서버가 정상 작동중입니다.</h1>
-      <p>데이터베이스도 연결되었습니다...</p>
-    </div>`)
+      .send(`<!DOCTYPE html>
+      <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+      </head>
+      <body>
+      
+      <div class="container mt-3">
+        <h2>서버 상태 메시지.</h2>
+        <div class="mt-4 p-5 bg-primary text-white rounded">
+          <h1>서버가 정상 작동중입니다.</h1> 
+          <p>데이터베이스도 연결되었습니다...</p> 
+        </div>
+      </div>
+      
+      </body>
+      </html>
+      `)
   })
 }
