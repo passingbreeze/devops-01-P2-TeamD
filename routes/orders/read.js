929ab747a94def2657610b69b7ff535bd0d732d1
ByeongJunis <guqudjun111@gmail.com>
2022-04-04 15:43:55 +0900

    API get 생성

--- routes/orders/read.js
+++ routes/orders/read.js
@@ -3,7 +3,12 @@ const { readAll } = require('../../model')
 
 module.exports = async function (fastify, opts) {
     fastify.get('/', async function (request, reply) {
-        const result = await readAll(this.mongo);
+        const result = await readAll(this.mongo,'order');
+        const result2 = await readAll(this.mongo,'restaurants');
+        //console.log("#####",result)
+        console.log("1234",result)
+        result[0].orderedMenu[0]._id=result2[2].menu[0]._id
+        result[0].orderedMenu[1]._id=result2[2].menu[1]._id
         
         if(result === []){
             reply
@@ -11,10 +16,14 @@ module.exports = async function (fastify, opts) {
               .header('Content-Type','application/json')
               .send("error : Not Found")
           }else{
+            const deliverStatus = result[0].deliveryInfo.status;
+            const estimatedTime = result[0].deliveryInfo.estimatedDeleveryTime;
+            const deliveryInfo = { deliverStatus, estimatedTime };
+            console.log(deliveryInfo);
            reply
             .code(200)
             .header('Content-Type','application/json')
-            .send(result)
+            .send({_id:result[0]._id,orderedMenu:result[0].orderedMenu,deliveryInfo:deliveryInfo})
           }
 
     }
